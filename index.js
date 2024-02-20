2653878724c55417a2b69f0994f0362ca6849111
yuan <wheathare@gmail.com>
2024-02-20 16:55:50 +0100

    add a while function for decouvert

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function demande_creation() {
       question2 = parseFloat(
         prompt("Veuillez saisir le montant que vous voulez entre 100€ et 2000€")
       );
-      console.log("fn", question2);
+      while (question2 < 100 || question2 > 2000) {
+        question2 = parseFloat(
+          prompt("Veuillez rentrer un montant valide entre 100€ et 2000€")
+        );
+      }
   
       if (question2 >= 100 && question2 <= 2000) {
         alert("Vous avez demandé un découvert de " + question2 + "€");
